f1662961c9ff99498676c6d7206ec34ff6863f83
August (Chris) <maythmcyt@mail.com>
2018-04-28 15:47:49 -0700

    Presence rotater.

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -1,4 +1,5 @@
 const Event = require('../structures/BaseEvent');
+const games = require('../../assets/games.json');
 
 class ReadyEvent extends Event {
     constructor(bot) {
@@ -12,12 +13,22 @@ class ReadyEvent extends Event {
 
         bot.log.info('Konata Izumi has connected via Discord!');
         bot.editStatus('online', {
-            name: `${bot.config.prefix}help | [${bot.guilds.size}] | Hewwo~`,
+            name: `${bot.config.prefix}help | [${bot.guilds.size}] | ${games[Math.floor(Math.random() * games.length)]}`,
             type: 0
+        });
+        setInterval(() => {
+            bot.editStatus('online', {
+                name: `${bot.config.prefix}help | [${bot.guilds.size}] | ${games[Math.floor(Math.random() * games.length)]}`,
+                type: 0
+            }, 60000);
         });
         bot.webhook.createMessage({
             title: "Konata Izumi ;; Connected via Discord",
             description: "Hello! I connected via Discord.",
+            fields: [{
+                name: "Statistics",
+                value: `\`\`\`ini\n[ Konata Izumi's Statistics ]\nShards: ${this.bot.shards.size}\nGuilds: ${this.bot.guilds.size}\nUsers: ${this.bot.users.size}\`\`\``
+            }],
             color: bot.utils.color
         });
     }
