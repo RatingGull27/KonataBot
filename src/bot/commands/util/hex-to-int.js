6a540f530e276f01a3b2b93434d61d463099122f
werewolf26 <45981158+werewolf26@users.noreply.github.com>
2019-05-26 21:31:50 -0400

    Add files via upload

--- src/bot/commands/util/hex-to-int.js
+++ src/bot/commands/util/hex-to-int.js
@@ -18,10 +18,10 @@ class HexToIntCommand extends Command {
     }
 
     async execute(msg, args) {
-        if (!args[0]) return msg.channel.createMessage('<:OwOThink:439675032910954496> **|** Provide a hex color.');
+        if (!args[0]) return msg.channel.createMessage('<:owoThink:582320118173007883> **|** Provide a hex color.');
         if (!/^[0-9A-F]{6}$/i.test(args[0].toString())) return msg.channel.createMessage(':x: **|** Invalid hex color! (Tip: Use the hex without the pound!)');
 
-        msg.channel.createMessage(`<:OwOThink:439675032910954496> **|** The integer version of \`#${args[0]}\` is: \`${parseInt(`0x${args[0]}`)}\`.`);
+        msg.channel.createMessage(`<:owoThink:582320118173007883> **|** The integer version of \`#${args[0]}\` is: \`${parseInt(`0x${args[0]}`)}\`.`);
     }
 }
 
--- src/bot/commands/util/make-guild-invite.js
+++ src/bot/commands/util/make-guild-invite.js
@@ -17,7 +17,7 @@ class MakeGuildInviteCommand extends Command {
     }
 
     async execute(msg, args) {
-        const m = await msg.channel.createMessage('<:OwOThink:439675032910954496> **|** Creating guild invite...');
+        const m = await msg.channel.createMessage('<:owoThink:582320118173007883> **|** Creating guild invite...');
         const createInvite = msg.channel.permissionsOf(this.bot.user.id).has('createInstantInvite');
         if (!createInvite) return m.edit(':x: **|** Can\'t create an invitation. (I need the `CREATE_INSTANT_INVITE` permission!)');
 
