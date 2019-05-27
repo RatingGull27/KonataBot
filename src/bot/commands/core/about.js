0856ba981a70a7771376e100898c4bbd22e18c0d
werewolf26 <45981158+werewolf26@users.noreply.github.com>
2019-05-26 21:49:07 -0400

    Add files via upload

--- src/bot/commands/core/about.js
+++ src/bot/commands/core/about.js
@@ -14,7 +14,7 @@ class AboutCommand extends Command {
     async execute(msg, args) {
         return msg.channel.createMessage({ embed: {
             title: "__**About Konata Izumi**__",
-            description: "<:KonataHi:438518453083701249> **Hello! I'm Konata, your weeb helper!**\n\t<:KonataOwO:438519335556874240> I provide you everything that you want in your server:\n\n__**Features**__\n\t:ribbon: Moderation commands!\n\t:ribbon: Utilitized commands\n\t:ribbon: and much more!\n\t:heart: I was made by <@280158289667555328>, <@387043512232968193>, and <@229552088525438977>",
+            description: "<:KonataHi:582320118592176138> **Hello! I'm Konata, your weeb helper!**\n\t<:KonataOwO:582320118680518676> I provide you everything that you want in your server:\n\n__**Features**__\n\t:ribbon: Moderation commands!\n\t:ribbon: Utilitized commands\n\t:ribbon: and much more!\n\t:heart: I was made by DELECTABLE Day Yay#0211, 9881youlove#0037",
             fields: [{
                 name: "Guilds",
                 value: this.bot.guilds.size,
