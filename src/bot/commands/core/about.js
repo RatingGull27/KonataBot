90f59ecfc7a8f898bef5067d407ff18a7ea49e3f
werewolf26 <45981158+werewolf26@users.noreply.github.com>
2020-05-17 04:11:31 -0400

    Update about.js

--- src/bot/commands/core/about.js
+++ src/bot/commands/core/about.js
@@ -14,7 +14,7 @@ class AboutCommand extends Command {
     async execute(msg, args) {
         return msg.channel.createMessage({ embed: {
             title: "__**About Konata Izumi**__",
-            description: "<:konataHi:710895044847992853> **Hello! I'm Konata, your weeb helper!**\n\t<:konataowo:710900227887857704> I provide you everything that you want in your server:\n\n__**Features**__\n\t:ribbon: Moderation commands!\n\t:ribbon: Utilitized commands\n\t:ribbon: and much more!\n\t:heart: I was made by DELECTABLE Day Yay#0211, 9881youlove#0037",
+            description: "<:konataHi:710895044847992853> **Hello! I'm Konata, your weeb helper!**\n\t<:konataowo:710900227887857704> I provide you everything that you want in your server:\n\n__**Features**__\n\t:ribbon: Moderation commands!\n\t:ribbon: Utilitized commands\n\t:ribbon: and much more!\n\t:heart: I was made by 9881youlove#0037\n\n__**Credits**__\n\t:heart:Here is the list of persons that have helped in any way to the creation of Konata Izumi:\n\t> auguwu => fist author to make this bot with the code\n\t> dragonfire => `Util#trimArray`, `Util#shorten`, and most of the search category commands,\n\t> Mayo => `reddit` command and most of the website core,\n\t> snarkyllama => `owoify`, `cringify`, and `avatar` commands,\n\t> discord.js team => `MessageCollector`, `Collector`, and `Collection` classes.,\n\t> ohlookitsderpy => `edits some code` only three commits,\n\t> YorkAARGH => `tiger`, `red-panda`, `penguin`, `panda`, and `lion` commands,\n\t> RatingGull27 => `nsfw` command and most of the fix up the on bot code and another thing",
             fields: [{
                 name: "Guilds",
                 value: this.bot.guilds.size,
