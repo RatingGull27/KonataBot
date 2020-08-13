ad86176a0479aa247a6abae79b13ad1fb8541edf
werewolf26 <45981158+werewolf26@users.noreply.github.com>
2020-08-13 10:03:50 -0400

    Update the nsfw command

--- src/bot/commands/nsfw/erofeet.js
+++ src/bot/commands/nsfw/erofeet.js
@@ -2,26 +2,26 @@ const Command = require('../../structures/BaseCommand');
 
 const data = require("../../lib/image/nsfw/erofeet");
 
-class erofeetCommand extends Command {
+class erofeetsCommand extends Command {
     constructor(bot) {
         super(bot, {
-            name: 'erofeet',
-            desc: 'erofeet a user!',
+            name: 'erofeets',
+            desc: 'erofeests a user!',
             category: 'nsfw',
             nsfwOnly: true,
             examples: [
-                '{prefix}erofeet',
+                '{prefix}erofeets',
             ],
             cooldown: 5
         });
     }
 
     async execute(msg, args) {
-        const ctx = await msg.channel.createMessage(':mag: **|** Grabbing a erofeet..');
+        const ctx = await msg.channel.createMessage(':mag: **|** Grabbing a erofeets..');
 
         await ctx.delete();
         msg.channel.createMessage(data[Math.floor(Math.random() * data.length)]);
     }
 }
 
-module.exports = erofeetCommand;
+module.exports = erofeetsCommand;
--- src/bot/commands/nsfw/nsfw-command-list.js
+++ src/bot/commands/nsfw/nsfw-command-list.js
@@ -1,5 +1,7 @@
 const Command = require('../../structures/BaseCommand');
 
+const data = require("../../lib/image/nsfw/nsfw-command-list");
+
 class nsfwCommand extends Command {
     constructor(bot) {
         super(bot, {
@@ -10,6 +12,10 @@ class nsfwCommand extends Command {
     }
 
     async execute(msg, args) {
+        const ctx = await msg.channel.createMessage(':mag: **|** Grabbing a nsfw-command-list..');
+
+        await ctx.delete();
+        msg.channel.createMessage(data[Math.floor(Math.random() * data.length)]);
         return msg.channel.createMessage({ embed: {
             title: "__**nsfw_command_list Konata Izumi**__",
             description: "\n\n__**nsfw**__\n\t*anal\n\t*bj\n\t*blowjob\n\t*boobs\n\t*cum\n\t*ero\n\t*erofeets\n\t*erokemo\n\t*feet\n\t*feetg\n\t*femdom\n\t*futanari\n\t*hentai\n\t*holoero\n\t*hololewd\n\t*keta\n\t*les\n\t*pussy\n\t*r34\n\t*solo\n\t*solog\n\t*tits\n\t*trap\n\t*yuri",
@@ -18,7 +24,7 @@ class nsfwCommand extends Command {
             },
             color: this.bot.utils.color
         }});
-    }
-}
+    };
+};
 
 module.exports = nsfwCommand;
