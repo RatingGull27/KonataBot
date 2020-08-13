f32b95836ffa0041e6c3f336b5e62f2f5f9c0c68
werewolf26 <45981158+werewolf26@users.noreply.github.com>
2020-08-13 06:10:32 -0400

    Update nsfw-command-list.js

--- src/bot/commands/nsfw/nsfw-command-list.js
+++ src/bot/commands/nsfw/nsfw-command-list.js
@@ -12,7 +12,7 @@ class nsfwCommand extends Command {
     async execute(msg, args) {
         return msg.channel.createMessage({ embed: {
             title: "__**nsfw_command_list Konata Izumi**__",
-            description: "\n\n__**nsfw**__\n\t*anal\n\t*bj\n\t*blowjob\n\t*boobs\n\t*cum\n\t*ero\n\t*erofeet\n\t*erok\n\t*erokemo\n\t*feet\n\t*feetg\n\t*femdom\n\t*futanari\n\t*gasm\n\t*hentai\n\t*holoero\n\t*hololewd\n\t*keta\n\t*les\n\t*lewd\n\t*pussy\n\t*r34\n\t*solo\n\t*solog\n\t*spank\n\t*tits\n\t*trap\n\t*yuri",
+            description: "\n\n__**nsfw**__\n\t*anal\n\t*bj\n\t*blowjob\n\t*boobs\n\t*cum\n\t*ero\n\t*erofeet\n\t*erok\n\t*erokemo\n\t*feet\n\t*feetg\n\t*femdom\n\t*futanari\n\t*hentai\n\t*holoero\n\t*hololewd\n\t*keta\n\t*les\n\t*lewd\n\t*pussy\n\t*r34\n\t*solo\n\t*solog\n\t*spank\n\t*tits\n\t*trap\n\t*yuri",
             footer: {
                 text: "HAHAHA! No NSFW tag! is a meme | Here some nsfw anime folder link: https://mega.nz/folder/ImgA0KKK | Konata Izumi » Your weeb helper!"
             },
